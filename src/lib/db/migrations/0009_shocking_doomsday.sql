e55d36d2bf4aeb2034bb043033af871ca35e39cf
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2026-01-05 20:20:34 +0100

    chore(db): squash migrations 0009 and 0010 into single migration
    Consolidate user_github_connections table migration with index creation

--- src/lib/db/migrations/0009_shocking_doomsday.sql
+++ src/lib/db/migrations/0009_shocking_doomsday.sql
@@ -11,3 +11,5 @@ CREATE TABLE "user_github_connections" (
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "user_github_connections_clerk_user_id_unique" UNIQUE("clerk_user_id")
 );
+--> statement-breakpoint
+ALTER TABLE "projects" ADD COLUMN "github_installation_id" integer;
